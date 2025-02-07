112dc7f2d07a1c0dd070ee0060f34f7e5be6a2ca
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-02-07 16:27:55 +0100

    update geo_data API

--- api/geo_data.py
+++ api/geo_data.py
@@ -1,10 +1,11 @@
 import requests
+from config import BASE_URL, API_KEY  # Import des variables de config
 
 # Permet d'obtenir la latitude et longitude des villes
 cities = ["Paris", "Le Caire", "New York", "Londres", "Tokyo"]
 
 for city in cities:
-    url = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid=f97e5e8433890fa465d0c519e73f49f3"
+    url = f"{BASE_URL}?q={city}&appid={API_KEY}"  # Utilisation des variables importées
     r = requests.get(url)
     data = r.json()
     result = data[0]
@@ -12,4 +13,3 @@ for city in cities:
     lat = result.get('lat')
     lon = result.get('lon')
     print(f"Ville: {name}, Latitude: {lat}, Longitude: {lon}")
-
