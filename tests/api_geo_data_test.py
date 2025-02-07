95b3ce2c24abcde9dccc60b7ece9f27b4d4dfc16
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-02-07 15:41:36 +0100

    clean test api geo_data

--- tests/api_geo_data_test.py
+++ tests/api_geo_data_test.py
@@ -1,6 +1,14 @@
-import requests
+# http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
-url ="http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=f97e5e8433890fa465d0c519e73f49f3"
+import sys
+import os
+
+# Ajouter le répertoire parent au chemin d'accès
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import requests
+from config import API_KEY, BASE_URL
+
+url =f"{BASE_URL}geo/1.0/direct?q=London&appid={API_KEY}"
 r = requests.get(url)
 data_test = r.json()
 print(data_test)
