b1d843a02099e5a292a2aae922992827d04f16de
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-02-11 13:53:36 +0100

    update geo_data api

--- api/geo_data.py
+++ api/geo_data.py
@@ -1,15 +1,43 @@
+import sys
+import os
+# Ajoute le répertoire parent au chemin d'accès (erreur 'attempted relative import with no known parent package')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import requests
-from config import BASE_URL, API_KEY  # Import des variables de config
+import csv
+from config import API_KEY, BASE_URL
 
-# Permet d'obtenir la latitude et longitude des villes
-cities = ["Paris", "Le Caire", "New York", "Londres", "Tokyo"]
+# Latitude et longitude des villes
+cities = [
+    "Paris", "Le Caire", "New York", "Londres", "Tokyo", "Delhi",
+    "Shanghai", "Los Angeles", "Moscou", "São Paulo", "Mexico",
+    "Pékin", "Istanbul", "Jakarta", "Séoul", "Bangkok", "Mumbai",
+    "Buenos Aires", "Lagos", "Hong Kong", "Singapour", "Sydney",
+    "Toronto", "Dubaï", "Rome"
+]
+
+geo_data = []
 
 for city in cities:
-    url = f"{BASE_URL}?q={city}&appid={API_KEY}"  # Utilisation des variables importées
+    url =f"{BASE_URL}geo/1.0/direct?q={city}&appid={API_KEY}"
     r = requests.get(url)
     data = r.json()
-    result = data[0]
-    name = result.get('name')
-    lat = result.get('lat')
-    lon = result.get('lon')
-    print(f"Ville: {name}, Latitude: {lat}, Longitude: {lon}")
+    # print(data)
+
+
+    # Récup géo data
+    geo_info = {
+        "city": city,
+        "country": data[0]["country"],
+        "lat": data[0]["lat"],
+        "long": data[0]["lon"]
+    }
+    # print(geo_info)
+    geo_data.append(geo_info)
+
+# add data to csv
+csv_file = "data_csv/geo_data.csv"
+headers = ["city","country","lat","long"]
+with open(csv_file, mode='w', newline='', encoding="utf-8") as file:
+    writer = csv.DictWriter(file, fieldnames=headers)
+    writer.writeheader()
+    writer.writerows(geo_data)
