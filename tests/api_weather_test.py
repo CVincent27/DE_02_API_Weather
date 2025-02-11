5250b1391018c04756255ec85558ef97c2444330
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-02-11 13:54:54 +0100

    update test weather api

--- tests/api_weather_test.py
+++ tests/api_weather_test.py
@@ -1,13 +1,13 @@
 # <!-- https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key} -->
-# import sys
-# import os
+import sys
+import os
 
-# # Ajouter le répertoire parent au chemin d'accès (erreur 'attempted relative import with no known parent package')
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# Ajoute le répertoire parent au chemin d'accès (erreur 'attempted relative import with no known parent package')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import requests
-from DE_02_API_Weather.config import API_KEY, BASE_URL
-# Récupère les infos météos
+from config import API_KEY, BASE_URL
 
+# Récupère les infos météos
 url = f"{BASE_URL}/data/2.5/weather?lat=44.34&lon=10.99&appid={API_KEY}"
 r = requests.get(url)
 data = r.json()
