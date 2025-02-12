394a544e0bf24d3fa9aa88e21c59aaea4e0ab306
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-02-12 12:56:44 +0100

    use csv geo data in weather file

--- api/weather.py
+++ api/weather.py
@@ -4,25 +4,24 @@ import os
 
 # Ajoute le répertoire parent au chemin d'accès (erreur 'attempted relative import with no known parent package')
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import requests
 import csv
+import pandas as pd
 from config import API_KEY, BASE_URL
 
-# Infos récupérées de geo_data
-cities = {
-    "Paris": {"lat": 48.8588897, "lon": 2.3200410217200766},
-    "Cairo": {"lat": 30.0443879, "lon": 31.2357257},
-    "New York": {"lat": 40.7127281, "lon": -74.0060152},
-    "London": {"lat": 51.5073219, "lon": -0.1276474},
-    "Tokyo": {"lat": 35.6828387, "lon": 139.7594549}
-}
-
+df = pd.read_csv("data_csv/geo_data.csv")
 weather_data = []
 
-for city_name, coordinates in cities.items():
-    url = f"{BASE_URL}/data/2.5/weather?lat={coordinates['lat']}&lon={coordinates['lon']}&appid={API_KEY}"
+# itérer sur chaque ligne
+for row in df.itertuples(index=False):
+    lat = row.lat
+    long = row.long
+    city_name = row.city
+    url = f"{BASE_URL}/data/2.5/weather?lat={lat}&lon={long}&units=metric&lang=fr&appid={API_KEY}"
     r = requests.get(url)
     data = r.json()
+
     
     # Récup data
     weather_info = {
@@ -31,14 +30,26 @@ for city_name, coordinates in cities.items():
         "temperature": data["main"]["temp"],
         "feels_like": data["main"]["feels_like"],
         "temperature_min": data["main"]["temp_min"],
-        "temperature_max": data["main"]["temp_max"]
+        "temperature_max": data["main"]["temp_max"],
+        "pressure": data["main"]["pressure"],
+        "humidity": data["main"]["humidity"],
+        "wind_speed": data["wind"]["speed"],
+        "wind_direction_deg": data["wind"]["deg"],
+        "rain": data.get("rain", {}).get("1h", 0), 
+        "snow": data.get("snow", {}).get("1h", 0),
+        "sunrise": data["sys"]["sunrise"],
+        "sunset": data["sys"]["sunset"],
+        "timezone": data["timezone"]
     }
     weather_data.append(weather_info)
 
-# add data to CSV file
+# add data csv weather
 csv_file = "data_csv/weather_data.csv"
-headers = ["city", "weather", "temperature", "feels_like", "temperature_min", "temperature_max"]
-with open(csv_file, mode='w', newline='') as file:
+headers = ["city", "weather", "temperature", "feels_like", "temperature_min", "temperature_max",
+           "pressure", "humidity", "wind_speed", "wind_direction_deg", "rain", "snow",
+           "sunrise", "sunset", "timezone"
+        ]
+with open(csv_file, mode='w', newline='', encoding="utf-8") as file:
     writer = csv.DictWriter(file, fieldnames=headers)
     writer.writeheader()
     writer.writerows(weather_data)
