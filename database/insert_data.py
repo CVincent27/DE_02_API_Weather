74e178bccf2333e7579bc24b755dd8dfb62947fd
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-02-13 14:32:36 +0100

    add data into weather table

--- database/insert_data.py
+++ database/insert_data.py
@@ -1,4 +1,6 @@
 import csv
+import sqlite3
+from config import DB_PATH
 
 def insert_data_from_csv(connection, csv_file):
     cursor = connection.cursor()
@@ -8,7 +10,15 @@ def insert_data_from_csv(connection, csv_file):
             cursor.execute("""
                 INSERT INTO cities (city, country) VALUES (?, ?)
             """, (row['city'], row['country']))
+
+            cursor.execute("""
+                INSERT INTO weather (weather, temperature,
+            feels_like, temperature_min, temperature_max) VALUES (?, ?, ?, ?, ?)
+            """, (row['weather'], row['temperature'],row['feels_like'],
+                  row['temperature_min'], row['temperature_max']))
     connection.commit()
 
 if __name__ == "__main__":
-    insert_data_from_csv()
+    connection = sqlite3.connect(DB_PATH)
+    insert_data_from_csv(connection, 'data_csv/weather_data.csv')
+    connection.close()
--- database/schema.py
+++ database/schema.py
@@ -13,7 +13,31 @@ def db_schema(connection):
             country TEXT NOT NULL
         )
     """)
-    
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS weather (
+            id INTEGER PRIMARY KEY,
+            city_id INTEGER,
+            weather TEXT,
+            temperature REAL,
+            feels_like REAL,
+            temperature_min REAL,
+            temperature_max REAL,
+            FOREIGN KEY (city_id) REFERENCES cities (id)
+        )
+    """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS conditions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            pressure INTEGER,
+            humidity INTEGER,
+            wind_speed REAL,
+            wind_direction_deg INTEGER,
+            cloud_cover INTEGER,
+            rain REAL,
+            snow REAL,
+            FOREIGN KEY (id) REFERENCES weather (id)
+        )
+    """)
     connection.commit()
 
 if __name__ == "__main__":
