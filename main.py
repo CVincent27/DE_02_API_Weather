0759d1e91a829dcd973044108e0ffa79f0b5e935
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-02-13 14:32:53 +0100

    clean main

--- main.py
+++ main.py
@@ -4,29 +4,24 @@ from api.weather import weather_data_fetch_save
 from database.schema import db_schema, connection, DB_PATH
 from database.create_tables import initialize_database
 from database.insert_data import insert_data_from_csv
-from database.queries import get_all_cities
-
-
+from database.queries import test_get_all_cities
 
 def main():
-
-    # Recup geoloc
+    #  geoloc
     geo_data_fetch_save()
-    # Recup data cities
+    #  data weather cities
     weather_data_fetch_save()
 
-    # Initialiser la base de données
+    # init db
     db_schema(connection)
     initialize_database(DB_PATH)
 
-    # Insérer des données à partir d'un fichier CSV
     insert_data_from_csv(connection, 'data_csv/weather_data.csv')
 
-    # Exécuter une requête
-    users = get_all_cities(connection)
-    print(users)
+    # queries
+    cities = test_get_all_cities(connection)
+    print(cities)
 
-    # Fermer la connexion
     connection.close()
 
 if __name__ == "__main__":
