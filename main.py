8b94a702af71b4b3f28772dfd7aab3651ce7977e
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-02-13 13:25:26 +0100

    update main, add api fonctions

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-import sqlite3
+from api.geo_data import geo_data_fetch_save
+from api.weather import weather_data_fetch_save
+
+from database.schema import db_schema, connection, DB_PATH
 from database.create_tables import initialize_database
 from database.insert_data import insert_data_from_csv
-from database.queries import get_all_users
-import os
+from database.queries import get_all_cities
+
 
 
 def main():
-    # Initialiser la base de données
-    db_path = os.path.join('database', 'data_weather.db')
-    initialize_database(db_path)
 
-    # Connexion à la base de données
-    connection = sqlite3.connect(db_path)
+    # Recup geoloc
+    geo_data_fetch_save()
+    # Recup data cities
+    weather_data_fetch_save()
+
+    # Initialiser la base de données
+    db_schema(connection)
+    initialize_database(DB_PATH)
 
     # Insérer des données à partir d'un fichier CSV
     insert_data_from_csv(connection, 'data_csv/weather_data.csv')
 
     # Exécuter une requête
-    users = get_all_users(connection)
+    users = get_all_cities(connection)
     print(users)
 
     # Fermer la connexion
