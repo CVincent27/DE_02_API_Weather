557fdddeaad4d89108a61726ee0d9bfe30196528
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-02-13 13:23:37 +0100

    update fonction geo_data_fetch_save

--- api/geo_data.py
+++ api/geo_data.py
@@ -7,37 +7,37 @@ import csv
 from config import API_KEY, BASE_URL
 
 # Latitude et longitude des villes
-cities = [
-    "Paris", "Le Caire", "New York", "Londres", "Tokyo", "Delhi",
-    "Shanghai", "Los Angeles", "Moscou", "São Paulo", "Mexico",
-    "Pékin", "Istanbul", "Jakarta", "Séoul", "Bangkok", "Mumbai",
-    "Buenos Aires", "Lagos", "Hong Kong", "Singapour", "Sydney",
-    "Toronto", "Dubaï", "Rome"
-]
+def geo_data_fetch_save():
+    cities = [
+        "Paris", "Le Caire", "New York", "Londres", "Tokyo", "Delhi",
+        "Shanghai", "Los Angeles", "Moscou", "São Paulo", "Mexico",
+        "Pékin", "Istanbul", "Jakarta", "Séoul", "Bangkok", "Mumbai",
+        "Buenos Aires", "Lagos", "Hong Kong", "Singapour", "Sydney",
+        "Toronto", "Dubaï", "Rome"
+    ]
 
-geo_data = []
+    geo_data = []
 
-for city in cities:
-    url =f"{BASE_URL}geo/1.0/direct?q={city}&appid={API_KEY}"
-    r = requests.get(url)
-    data = r.json()
-    # print(data)
+    for city in cities:
+        url =f"{BASE_URL}geo/1.0/direct?q={city}&appid={API_KEY}"
+        r = requests.get(url)
+        data = r.json()
+        # print(data)
 
+        geo_info = {
+            "city": city,
+            "country": data[0]["country"],
+            "lat": data[0]["lat"],
+            "long": data[0]["lon"]
+        }
+        geo_data.append(geo_info)
 
-    # Récup géo data
-    geo_info = {
-        "city": city,
-        "country": data[0]["country"],
-        "lat": data[0]["lat"],
-        "long": data[0]["lon"]
-    }
-    # print(geo_info)
-    geo_data.append(geo_info)
+    csv_file = "data_csv/geo_data.csv"
+    headers = ["city","country","lat","long"]
+    with open(csv_file, mode='w', newline='', encoding="utf-8") as file:
+        writer = csv.DictWriter(file, fieldnames=headers)
+        writer.writeheader()
+        writer.writerows(geo_data)
 
-# add data to csv
-csv_file = "data_csv/geo_data.csv"
-headers = ["city","country","lat","long"]
-with open(csv_file, mode='w', newline='', encoding="utf-8") as file:
-    writer = csv.DictWriter(file, fieldnames=headers)
-    writer.writeheader()
-    writer.writerows(geo_data)
+if __name__ == "__main__":
+    geo_data_fetch_save()
