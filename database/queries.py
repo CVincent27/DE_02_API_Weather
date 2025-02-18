40704c4ee1182bb5ec28f435a3b9c18e39cfc336
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-02-18 13:20:25 +0100

    update query test

--- database/queries.py
+++ database/queries.py
@@ -1,4 +1,4 @@
 def test_get_all_cities(connection):
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM weather LIMIT 5")
+    cursor.execute("SELECT * FROM conditions LIMIT 5")
     return cursor.fetchall()
