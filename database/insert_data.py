77f98ec1b64cd09aaafa88e1b4c58948a9e4f005
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-02-18 13:20:15 +0100

    add data solar_events

--- database/insert_data.py
+++ database/insert_data.py
@@ -16,6 +16,17 @@ def insert_data_from_csv(connection, csv_file):
             feels_like, temperature_min, temperature_max) VALUES (?, ?, ?, ?, ?)
             """, (row['weather'], row['temperature'],row['feels_like'],
                   row['temperature_min'], row['temperature_max']))
+            
+            cursor.execute("""
+                INSERT INTO conditions (pressure, humidity,
+            wind_speed, wind_direction_deg, cloud_cover, rain, snow) VALUES (?, ?, ?, ?, ?, ?, ?)
+            """, (row['pressure'], row['humidity'],row['wind_speed'],
+                  row['wind_direction_deg'], row['cloud_cover'], row['rain'], row['snow']))
+            
+            cursor.execute("""
+                INSERT INTO solar_events (sunrise, sunset, timezone, utc, sunrise_time, sunset_time) VALUES (?, ?, ?, ?, ?, ?)
+            """, (row['sunrise'], row['sunset'],row['timezone'],row['utc'],
+                  row['sunrise_time'], row['sunset_time']))
     connection.commit()
 
 if __name__ == "__main__":
