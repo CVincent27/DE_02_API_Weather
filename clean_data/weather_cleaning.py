f2350860f369c3ed2854ebd6c14af0068b0a6ca7
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-02-18 13:19:50 +0100

    add timezone UTC

--- clean_data/weather_cleaning.py
+++ clean_data/weather_cleaning.py
@@ -1,26 +1,34 @@
 from datetime import datetime, timedelta
 import pandas as pd
 
-df = pd.read_csv("data_csv/weather_data.csv")
-sunrise_times = []
-sunset_times = []
 
-for col in df.itertuples(index=False):
-    sunrise_utc = col.sunrise
-    sunset_utc = col.sunset
-    timezone_shift = col.timezone
+def weather_data_cleaning():
+    df = pd.read_csv("data_csv/weather_data.csv")
+    sunrise_times = []
+    sunset_times = []
+    timezones = []
 
-    # Transfo UTC en heure locale
-    sunrise_utc = datetime.fromtimestamp(sunrise_utc)
-    sunset_utc = datetime.fromtimestamp(sunset_utc)
+    for col in df.itertuples(index=False):
+        sunrise_utc = col.sunrise
+        sunset_utc = col.sunset
+        timezone_shift = col.timezone
+        timezones.append(timezone_shift // 3600)
 
-    # decal horaire
-    sunrise_local = sunrise_utc + timedelta(seconds=timezone_shift)
-    sunset_local = sunset_utc + timedelta(seconds=timezone_shift)
+        # Transfo UTC en heure locale
+        sunrise_utc = datetime.fromtimestamp(sunrise_utc)
+        sunset_utc = datetime.fromtimestamp(sunset_utc)
 
-    sunrise_times.append(sunrise_local.strftime('%H:%M'))
-    sunset_times.append(sunset_local.strftime('%H:%M'))
+        # decal horaire
+        sunrise_local = sunrise_utc + timedelta(seconds=timezone_shift)
+        sunset_local = sunset_utc + timedelta(seconds=timezone_shift)
 
-df['sunrise_time'] = sunrise_times
-df['sunset_time'] = sunset_times
-df.to_csv("data_csv/weather_data.csv", index=False)
+        sunrise_times.append(sunrise_local.strftime('%H:%M'))
+        sunset_times.append(sunset_local.strftime('%H:%M'))
+
+    df['utc'] = timezones
+    df['sunrise_time'] = sunrise_times
+    df['sunset_time'] = sunset_times
+    df.to_csv("data_csv/weather_data.csv", index=False)
+
+if __name__ == "__main__":
+    weather_data_cleaning()
